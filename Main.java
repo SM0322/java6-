888e6166f843e7aa28bfbd2da57f327a786bcab2
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-25 01:56:12 +0000

    6-1

---
+++ Main.java
@@ -0,0 +1,11 @@
+import comment.Zenhan;
+import comment.Kouhan;
+
+public class Main {
+    public static void main(String[] args) {
+      Zenhan.doWarusa();
+      Zenhan.doTogame();
+      Kouhan.callDeae();
+      Kouhan.showMondokoro();
+    }
+}
---
+++ comment/Kouhan.java
@@ -0,0 +1,9 @@
+package comment;
+public class Kouhan {
+  public static void callDeae() {
+    System.out.println("ええい小癪な。九会ものだ！であえい。");
+  }
+  public static void showMondokoro() {
+    System.out.println("このもんどころが目に入らぬか");
+  }
+}
