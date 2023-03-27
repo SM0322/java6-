9f45a909c1a1dd5fefe41b3d0a849f192372c84f
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-27 23:38:22 +0000

    フィールドにクラス型を宣言する方法

--- Hero.java
+++ Hero.java
@@ -1,24 +1,29 @@
 public class Hero {
   String name;
   int hp;
+  Sword sword;
   public void attack() {
+    System.out.println(this.name + "は攻撃した!");
+    System.out.println("敵に5ポイントダメージを与えた！");
+  }
+  // public void attack() {
     
-  }
-  public void sleep() {
-    this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！");
-  }
-  public void sit(int sec) {
-    this.hp += sec;
-    System.out.println(this.name + "は、" + sec + "秒座った！");
-    System.out.println("HPが" + sec + "ポイント回復した！");
-  }
-  public void slip() {
-    this.hp -= 5;
-    System.out.println(this.name + "は、転んだ！");
-    System.out.println("HPは" + this.hp + "になった！");
-  }
-  public void run() {
-    System.out.println(this.name + "は、逃げだした！");
-  }
+  // }
+  // public void sleep() {
+  //   this.hp = 100;
+  //   System.out.println(this.name + "は、眠って回復した！");
+  // }
+  // public void sit(int sec) {
+  //   this.hp += sec;
+  //   System.out.println(this.name + "は、" + sec + "秒座った！");
+  //   System.out.println("HPが" + sec + "ポイント回復した！");
+  // }
+  // public void slip() {
+  //   this.hp -= 5;
+  //   System.out.println(this.name + "は、転んだ！");
+  //   System.out.println("HPは" + this.hp + "になった！");
+  // }
+  // public void run() {
+  //   System.out.println(this.name + "は、逃げだした！");
+  // }
 }
--- Main.java
+++ Main.java
@@ -15,7 +15,9 @@ public class Main {
     Hero h1;
     h1 = new Hero();
     h1.hp = 100;
-    System.out.println(h1.hp);
+    h1.name = "佐藤";
+    h1.attack();
+    System.out.println(h1.hp + h1.name);
     Hero h2;
     h2 = h1;
     h2.hp = 200;
