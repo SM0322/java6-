d8e4d8daa683fba3d816060bf703b979110ab14b
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-27 00:49:29 +0000

    オブジェクト指向:hero class　メソッド追加

--- Hero.java
+++ Hero.java
@@ -5,13 +5,18 @@ public class Hero {
     
   }
   public void sleep() {
-    
+    this.hp = 100;
+    System.out.println(this.name + "は、眠って回復した！")
   }
   public void sit(int sec) {
-    
+    this.hp += sec;
+    System.out.println(this.name + "は、" + sec + "秒座った！")
+    System.out.println("HPが" + sec + "ポイント回復した！")
   }
   public void slip() {
-    
+    this.hp -= 5;
+    System.out.println(this.name + "は、転んだ！")
+    System.out.println("HPは" + this.hp + "になった！")
   }
   public void run() {
     
