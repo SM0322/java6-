80a7d9bb77c9bdd71fd9aeb4d866b72dcc78fdb9
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-27 00:59:53 +0000

    オブジェクト指向:Battleに命令追加

--- Battle.java
+++ Battle.java
@@ -4,5 +4,13 @@ public class Battle {
     //   お化けキノコよ、この世界に生まれよ！
     //   勇者よ、戦え！
     //   お化けキノコよ、逃げろ！
+    Hero h = new Hero();
+    h.name = "湊";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を、生み出しました！");
+    h.sit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
     }
 }
--- Hero.java
+++ Hero.java
@@ -6,19 +6,19 @@ public class Hero {
   }
   public void sleep() {
     this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！")
+    System.out.println(this.name + "は、眠って回復した！");
   }
   public void sit(int sec) {
     this.hp += sec;
-    System.out.println(this.name + "は、" + sec + "秒座った！")
-    System.out.println("HPが" + sec + "ポイント回復した！")
+    System.out.println(this.name + "は、" + sec + "秒座った！");
+    System.out.println("HPが" + sec + "ポイント回復した！");
   }
   public void slip() {
     this.hp -= 5;
-    System.out.println(this.name + "は、転んだ！")
-    System.out.println("HPは" + this.hp + "になった！")
+    System.out.println(this.name + "は、転んだ！");
+    System.out.println("HPは" + this.hp + "になった！");
   }
   public void run() {
-    
+    System.out.println(this.name + "は、逃げだした！");
   }
 }
