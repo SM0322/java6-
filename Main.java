9ea65822854e5e94d6fa06b49fa1a58a2fa7b7b0
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-27 23:44:37 +0000

    has-aの関係

--- Main.java
+++ Main.java
@@ -12,12 +12,16 @@
 
 public class Main {
   public static void main (String[] args) {
+    Sword s = new Sword();
+    s.name = "ぢゅらんだる";
+    s.damage = 10;
     Hero h1;
     h1 = new Hero();
     h1.hp = 100;
     h1.name = "佐藤";
+    h1.sword = s;
     h1.attack();
-    System.out.println(h1.hp + h1.name);
+    System.out.println(h1.hp + h1.sword.name);
     Hero h2;
     h2 = h1;
     h2.hp = 200;
