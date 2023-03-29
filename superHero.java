6f665f0e34fe3dba1ec0da2994cb4811ea136bf1
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-29 04:06:27 +0000

    superで」親クラスからメソッドを呼び出す

--- superHero.java
+++ superHero.java
@@ -5,15 +5,9 @@ public class superHero extends Hero {
   public void attack(Matango m) {
     // System.out.println(this.name + "は" + this.sword.name + "で攻撃した!");
     // System.out.println("敵に" + this.sword.damage + "ポイントダメージを与えた！");
-    System.out.println(this.name + "は攻撃した!");
-    m.hp -= 5;
-    System.out.println("敵に５ポイントダメージを与えた！");
-    System.out.println(m.hp);
+    super.attack(m);
     if (this.flying) {
-      System.out.println(this.name + "は攻撃した!");
-      m.hp -= 5;
-      System.out.println("敵に５ポイントダメージを与えた！");
-      System.out.println(m.hp);
+      super.attack(m);
     }
   }
   public void run() {
