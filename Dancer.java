f88ca37e2ae6ab50cca9f5f9bb08a70a0a049115
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-31 02:52:24 +0000

    抽象メソッドをオーバーライドすることでエラー回避

--- Dancer.java
+++ Dancer.java
@@ -2,4 +2,9 @@ public class Dancer extends Character {
   public void dance() {
     System.out.println(this.name + "は踊った!");
   }
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃!");
+    System.out.println("敵に１０ポイントのダメージ!");
+    m.hp -= 10;
+  }
 }
