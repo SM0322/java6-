771cc765598a4c21d4a467e42136fd4218447d3b
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-31 05:36:48 +0000

    11-1課題修了

---
+++ Computer.java
@@ -0,0 +1,9 @@
+public class Computer {
+  String makerName;
+  public Computer
+    (String name, int price, String color, String makerName) {
+      super(name, price, color)
+      this.makerName = makerName;
+  }
+  public String getMakerName() { return this.makerName; }
+}
