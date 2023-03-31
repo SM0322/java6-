9243e48a92c8bdf262a870d4d4395cd91081383d
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-31 02:23:02 +0000

    継承元クラスの不都合

---
+++ Character.java
@@ -0,0 +1,13 @@
+public class Character {
+  String name;
+  int hp;
+  public void run() {
+    System.out.println(this.name + "は逃げ出した！");
+    m.hp = -= ??;
+  }
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃!");
+    m.hp = -= ??;
+    System.out.println("敵に??ポイントのダメージを与えた!")
+  }
+}
