4eaee956a3cc324f86e187cbad3089a30df10ec8
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-31 02:45:55 +0000

    抽象化したメソッドに{}以降は必要ない

--- Character.java
+++ Character.java
@@ -6,13 +6,14 @@ public abstract class Character {
     System.out.println(this.name + "は逃げ出した！");
   }
 //   abstractをvoidの前に入れることでattack()を抽象化した
-  public abstract void attack(Matango m) {
-    // System.out.println(this.name + "の攻撃!");
-    // m.hp = -= ??;
-    // System.out.println("敵に??ポイントのダメージを与えた!")
-    // attackは必ず必要だがキャラごとで内容が違う。その不都合対策の１つにattack()メソッドを空にしておく.
-    // そして未来の開発者がattackをオーバーロードすればよい
-  }
+  public abstract void attack(Matango m);
+//   {
+//     // System.out.println(this.name + "の攻撃!");
+//     // m.hp = -= ??;
+//     // System.out.println("敵に??ポイントのダメージを与えた!")
+//     // attackは必ず必要だがキャラごとで内容が違う。その不都合対策の１つにattack()メソッドを空にしておく.
+//     // そして未来の開発者がattackをオーバーロードすればよい
+//   }
   public Character() {
     this.hp = 100;
     this.name = "湊";
